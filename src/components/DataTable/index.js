6917f837a4f1b0af69bfda8e4eac67e3c40095b8
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-03-20 01:45:33 +0500

    Content View added

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -15,7 +15,7 @@ class DataTable extends Component {
   renderTableHeader = () => {
     let { headers } = this.state;
     headers.sort((a, b) => {
-      if (a.index > a.index) return 1;
+      if (a.index > b.index) return 1;
       return -1;
     });
     let headerView = headers.map((header, index) => {
@@ -36,10 +36,45 @@ class DataTable extends Component {
     });
     return headerView;
   };
+
+  renderNoData = () => {
+    return (
+      <tr>
+        <td colSpan={this.props.headers.length}>{this.noData}</td>
+      </tr>
+    );
+  };
+  renderContent = () => {
+    let { headers, data } = this.state;
+    let contentView = data.map((row, rowIndex) => {
+      let id = row[this.keyField];
+      let tds = headers.map((header, index) => {
+        let content = row[header.accessor];
+        let cell = header.cell;
+        if (cell) {
+          if (typeof cell === "object") {
+            if (cell.type === "image" && content) {
+              content = <img style={cell.style} src={content} />;
+            }
+          } else if (typeof cell === "function") {
+            content = cell(content);
+          }
+        }
+        return (
+          <td key={index} data-id={id} data-row={rowIndex}>
+            {content}
+          </td>
+        );
+      });
+      return <tr key={rowIndex}>{tds}</tr>;
+    });
+    return contentView;
+  };
   renderTable = () => {
     let title = this.props.title || "DataTable";
     let headerView = this.renderTableHeader();
-    let contentView = "Content goes here";
+    let contentView =
+      this.state.data.length > 0 ? this.renderContent() : this.renderNoData();
 
     return (
       <table className="data-inner-table">
@@ -47,10 +82,7 @@ class DataTable extends Component {
         <thead>
           <tr>{headerView}</tr>
         </thead>
-        <tbody>
-          {!this.state.data.length && this.noData}
-          {this.state.data && contentView}
-        </tbody>
+        <tbody>{contentView}</tbody>
       </table>
     );
   };
