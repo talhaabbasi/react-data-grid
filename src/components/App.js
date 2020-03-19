a9d002b3d718577c04c238f7daa55d8727ebcbf4
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-03-19 14:44:23 +0500

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    let model = {};
+  }
+  render() {
+    return <div className="App"></div>;
+  }
+}
+
+export default App;
